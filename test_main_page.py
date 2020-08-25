ac0c64bc1905679528180270023f46194bfd3c54
HarbacheuskayaAnna <harbacheuskaya.anna@gmail.com>
2020-08-25 12:28:18 +0300

    Updated tests with exceptions, added should_be_login_link test

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from .pages.main_page import MainPage
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -6,11 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
