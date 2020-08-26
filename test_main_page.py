071fbd42dbcc86a4942a5ace81992f95e5754280
HarbacheuskayaAnna <harbacheuskaya.anna@gmail.com>
2020-08-26 15:49:48 +0300

    LoginPage locators added

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,18 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    time.sleep(20)
+    url = browser.current_url
+    page = LoginPage(browser, url)
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
 
 
 def test_guest_should_see_login_link(browser):
